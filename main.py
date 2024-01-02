4187efd63590a3cac47a9f35200ed481890620a2
Soulbolt <soulbolt@gmail.com>
2024-01-02 00:06:05 -0700

    implemented a for loop to go through each movie title in reverse and create file with the top 100 in ascending order.

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from bs4 import BeautifulSoup
 response = requests.get(url="https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 response.raise_for_status
 content = response.text
-
+# Store websites content
 soup = BeautifulSoup(content, "html.parser")
-
+# Get movie titles
 movie_titles = soup.find_all(name="h3", class_="title")
-print(movie_titles)
+
+# For loop to create file if it does not exist and add movie tiles on reverse to view titles from 1-100 into a txt file.
+for title in reversed(movie_titles):
+    with open("movies.txt", "a", encoding="utf-8") as file:
+        file.write(title.getText()+"\n")
